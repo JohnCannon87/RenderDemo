67dcd5545640002520a4b7a2401d1e56bdeba4e9
John Cannon <john.arc.cannon@gmail.com>
2024-06-30 19:57:05 +0100

    Logging Out DB parameters

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,15 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.logging.AppContextEventListener;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication app = new SpringApplication(DemoApplication.class);
+		app.addInitializers(new AppContextEventListener());
+		app.run(args);
 	}
 
 }
--- src/main/java/com/example/demo/logging/AppContextEventListener.java
+++ src/main/java/com/example/demo/logging/AppContextEventListener.java
@@ -1,50 +1,36 @@
 package com.example.demo.logging;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.Arrays;
+import java.util.stream.StreamSupport;
 
-import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.MapPropertySource;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.AbstractEnvironment;
+import org.springframework.core.env.EnumerablePropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.MutablePropertySources;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
-public class AppContextEventListener {
+public class AppContextEventListener implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
-	@EventListener
-	public void handleContextRefreshed(ContextRefreshedEvent event) {
-		printActiveProperties((ConfigurableEnvironment) event.getApplicationContext().getEnvironment());
-	}
-
-	private void printActiveProperties(ConfigurableEnvironment env) {
-
-		System.out.println("************************* ACTIVE APP PROPERTIES ******************************");
-
-		List<MapPropertySource> propertySources = new ArrayList<>();
-
-		env.getPropertySources().forEach(it -> {
-			if (it instanceof MapPropertySource && it.getName().contains("applicationConfig")) {
-				propertySources.add((MapPropertySource) it);
-			}
-		});
-
-		propertySources.stream()
-				.map(propertySource -> propertySource.getSource().keySet())
-				.flatMap(Collection::stream)
+	@Override
+	public void initialize(ConfigurableApplicationContext applicationContext) {
+		final Environment env = applicationContext.getEnvironment();
+		log.info("====== Environment and configuration ======");
+		log.info("Active profiles: {}", Arrays.toString(env.getActiveProfiles()));
+		final MutablePropertySources sources = ((AbstractEnvironment) env).getPropertySources();
+		StreamSupport.stream(sources.spliterator(), false)
+				.filter(ps -> ps instanceof EnumerablePropertySource)
+				.map(ps -> ((EnumerablePropertySource) ps).getPropertyNames())
+				.flatMap(Arrays::stream)
 				.distinct()
 				.sorted()
-				.forEach(key -> {
-					try {
-						System.out.println(key + "=" + env.getProperty(key));
-					} catch (Exception e) {
-						log.warn("{} -> {}", key, e.getMessage());
-					}
-				});
-		System.out.println("******************************************************************************");
+				// .filter(prop -> prop.contains("spring.datasource") || prop.contains("DB"))
+				.forEach(prop -> log.info("{}: {}", prop, env.getProperty(prop)));
+		log.info("===========================================");
 	}
 }
